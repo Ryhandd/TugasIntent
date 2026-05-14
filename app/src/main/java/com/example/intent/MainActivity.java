77c1b4cd1ae77bc0a7e34a4ffcb8263a86c82fd1
Ryhandd <rayhandwumu6@gmail.com>
2026-05-15 01:39:02 +0800

    first commit

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.intent;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.blue_dark));
+        }
+        btnPindah = findViewById(R.id.btnPindah);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
